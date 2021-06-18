07f06657105694c3eb144f89961af1fb5c18db9b
Work <k>
2021-06-18 22:01:06 +0700

    home_work_1

--- src/HWork_1/HomeWorkApp.java
+++ src/HWork_1/HomeWorkApp.java
@@ -2,6 +2,49 @@ package HWork_1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
     }
+
+    public static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = 3;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 1;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 45;
+        int b = 73;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
 }
