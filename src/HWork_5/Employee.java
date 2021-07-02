2c105ce56facbd034d9c472371324f73546eff3c
Alexey <traf10@bk.ru>
2021-07-03 00:01:22 +0700

    home_work_5

--- src/HWork_5/Employee.java
+++ src/HWork_5/Employee.java
@@ -1,5 +1,87 @@
 package HWork_5;
 
 public class Employee {
+    /** 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.*/
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
+    /** 2. Конструктор класса должен заполнять эти поля при создании объекта.*/
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+
+    }
+    /** 3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.*/
+    public void info() {
+        System.out.printf("ФИО: %s\n Должность: %s\n Email: %s\n Телефон: %s\n " +
+                "Зарплата: %d\n Возраст: %d", fullName, position, email, phoneNumber, salary, age);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
 }
