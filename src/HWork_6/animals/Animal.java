d049d03facb31f37b8d4d4fdc7f7a2a79ff3d58b
Alexey <traf10@bk.ru>
2021-07-05 20:50:17 +0700

    home_work_6

--- src/HWork_6/animals/Animal.java
+++ src/HWork_6/animals/Animal.java
@@ -1,6 +1,31 @@
 package HWork_6.animals;
 
 
-public class Animal {
 
+public class Animal {
+    String name;
+    String color;
+    int age;
+    static int bigCount;
+
+
+    public Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        bigCount++;
+    }
+
+
+    public void print() {
+        System.out.println("Создано животных: " + bigCount);
+    }
+
+    public void run(double runDistance) {
+        System.out.printf("%s пробежал %.2fм\n", name, runDistance);
+    }
+
+    public void swim(double swimDistance) {
+        System.out.printf("%s проплыл %.2fм\n", name, swimDistance);
+    }
 }
--- src/HWork_6/animals/Dog.java
+++ src/HWork_6/animals/Dog.java
@@ -1,6 +1,39 @@
 package HWork_6.animals;
 
-public class Dog  {
+public class Dog extends Animal {
+
+    static int count1 = 0;
 
 
+    public Dog(String name, String color, int age) {
+        super(name, color, age);
+        count1++;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Собак создано: " + count1);
+    }
+
+    @Override
+    public void run(double runDistance) {
+        if (runDistance < 0) {
+            System.out.println("Собаки задом наперёд не бегают!");
+        } else if (runDistance > 0 && runDistance <= 500) {
+            System.out.printf("%s пробежал %.2fм\n", name, runDistance);
+        } else {
+            System.out.printf("%s не может столько пробежать, ни один пёс на свете не смог бы!\n", name);
+        }
+    }
+
+    @Override
+    public void swim(double swimDistance) {
+        if (swimDistance < 0) {
+            System.out.println("Собаки задом наперёд не плаваают!");
+        } else if (swimDistance > 0 && swimDistance <= 10) {
+            System.out.printf("%s проплыл %.2fм\n", name, swimDistance);
+        } else {
+            System.out.printf("%s не может столько проплыть!\n", name);
+        }
+    }
 }
