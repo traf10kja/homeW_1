26b8433b7121653c653be6a07a65aa7b82bae835
Alexey <traf10@bk.ru>
2021-07-08 13:33:54 +0700

    home_work_7

--- src/HWork_7/Plate.java
+++ src/HWork_7/Plate.java
@@ -1,4 +1,30 @@
 package HWork_7;
 
 public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void decrease(Cat cat) {
+        if (food > cat.getAppetite()) {
+            System.out.println(cat.checkHungry(this) + " не голодна");
+            food = food - cat.getAppetite();
+            return;
+        } else {
+            System.out.println(cat.checkHungry(this) + " остался голоден");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Plate{" +
+                "food=" + food +
+                '}';
+    }
+
+    public int getFood() {
+        return food;
+    }
 }
