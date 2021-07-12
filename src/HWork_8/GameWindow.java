aa2c41af11f79380ed24d322f31fd07d08e46018
Alexey <traf10@bk.ru>
2021-07-12 21:19:49 +0700

    home_work_8

--- src/HWork_8/GameWindow.java
+++ src/HWork_8/GameWindow.java
@@ -1,4 +1,45 @@
 package HWork_8;
 
-public class GameWindow {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow extends JFrame {
+
+    private BattleMap battleMap;
+    private Setting setting;
+
+    public GameWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(500, 250, 500, 400);
+        setTitle("TicTacToe");
+
+
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+
+        JButton button = new JButton("Start new game");
+        panel.add(button, BorderLayout.NORTH);
+
+        JButton buttonExit = new JButton("Exit");
+        panel.add(buttonExit, BorderLayout.NORTH);
+        add(panel, BorderLayout.SOUTH);
+
+        battleMap = new BattleMap(this);
+        add(battleMap, BorderLayout.CENTER);
+
+        setting = new Setting(this);
+
+        button.addActionListener(e -> setting.setVisible(true));
+
+        buttonExit.addActionListener(e -> System.exit(0));
+
+
+        setVisible(true);
+    }
+
+    void startNewGame(int fieldSize, int winLength) {
+        battleMap.startNewGame(fieldSize,winLength);
+    }
+
+    }
--- src/HWork_8/TicTacToe.java
+++ src/HWork_8/TicTacToe.java
@@ -2,5 +2,6 @@ package HWork_8;
 
 public class TicTacToe {
     public static void main(String[] args) {
+        new GameWindow();
     }
 }
